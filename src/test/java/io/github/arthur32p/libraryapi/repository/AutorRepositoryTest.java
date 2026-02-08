36e9fb6d8f388bb2a833cfdda0bcad11da454bac
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-02-08 11:45:38 -0300

    Utilizando testes para as operações de listar, contar, deletar por id e deletar por objeto na entidade Autor

--- src/test/java/io/github/arthur32p/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/arthur32p/libraryapi/repository/AutorRepositoryTest.java
@@ -2,10 +2,12 @@ package io.github.arthur32p.libraryapi.repository;
 
 import io.github.arthur32p.libraryapi.model.Autor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,7 +20,7 @@ public class AutorRepositoryTest {
     @Test
     public void salvarTest(){
         Autor autor = new Autor();
-        autor.setNome("Maria");
+        autor.setNome("João");
         autor.setNacionalidade("Brasileira");
         autor.setDataNascimento(LocalDate.of(1950, 3, 21));
 
@@ -42,5 +44,29 @@ public class AutorRepositoryTest {
 
             repository.save(autorEncontrado);
         }
+    }
+
+    @Test
+    public void listarTest(){
+        List<Autor> lista = repository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores " + repository.count());
+    }
+
+    @Test
+    public void deletePorIdTest(){
+        var id = UUID.fromString("b508f0d6-d84c-4bfb-a5c3-312d0b665157");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("107ffd05-c6d3-45b0-9c62-9e321302ea21");
+        var autor = repository.findById(id).get();
+        repository.delete(autor);
     }
 }
