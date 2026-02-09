2f8984f3cf9c03447221a04fe292b8c489136353
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-02-09 09:58:33 -0300

    Adicionando um autor com seus livros

--- src/main/java/io/github/arthur32p/libraryapi/model/Autor.java
+++ src/main/java/io/github/arthur32p/libraryapi/model/Autor.java
@@ -26,13 +26,7 @@ public class Autor {
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
 
-    // @OneToMany(mappedBy = "autor")
-    @Transient
+    @OneToMany(mappedBy = "autor")
     private List<Livro> livros;
-
-    @Deprecated
-    public Autor(){
-
-    }
 
 }
--- src/test/java/io/github/arthur32p/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/arthur32p/libraryapi/repository/AutorRepositoryTest.java
@@ -1,12 +1,15 @@
 package io.github.arthur32p.libraryapi.repository;
 
 import io.github.arthur32p.libraryapi.model.Autor;
+import io.github.arthur32p.libraryapi.model.GeneroLivro;
+import io.github.arthur32p.libraryapi.model.Livro;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,6 +19,9 @@ public class AutorRepositoryTest {
 
     @Autowired
     AutorRepository repository;
+
+    @Autowired
+    LivroRepository livroRepository;
 
     @Test
     public void salvarTest(){
@@ -68,5 +74,37 @@ public class AutorRepositoryTest {
         var id = UUID.fromString("107ffd05-c6d3-45b0-9c62-9e321302ea21");
         var autor = repository.findById(id).get();
         repository.delete(autor);
+    }
+
+    @Test
+    void salvarAutorComLivrosTest(){
+        Autor autor = new Autor();
+        autor.setNome("Antonio");
+        autor.setNacionalidade("Estadunidense");
+        autor.setDataNascimento(LocalDate.of(1991, 2, 12));
+
+        Livro livro = new Livro();
+        livro.setIsbn("87642-98742");
+        livro.setPreco(BigDecimal.valueOf(50));
+        livro.setGenero(GeneroLivro.MISTERIO);
+        livro.setTitulo("Livro Livro");
+        livro.setDataPublicacao(LocalDate.of(2014, 7, 3));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("87322-12342");
+        livro2.setPreco(BigDecimal.valueOf(70));
+        livro2.setGenero(GeneroLivro.FICCAO);
+        livro2.setTitulo("2 Livro");
+        livro2.setDataPublicacao(LocalDate.of(2014, 7, 3));
+        livro2.setAutor(autor);
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        repository.save(autor);
+
+        livroRepository.saveAll(autor.getLivros());
     }
 }
