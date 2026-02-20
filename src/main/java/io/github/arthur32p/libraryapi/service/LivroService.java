d232fb2e502f618fed4dbc349b2436092b825c93
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-02-20 20:39:06 -0300

    Criando estruturas básicas da API de Livro

---
+++ src/main/java/io/github/arthur32p/libraryapi/service/LivroService.java
@@ -0,0 +1,12 @@
+package io.github.arthur32p.libraryapi.service;
+
+import io.github.arthur32p.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LivroService {
+
+    private final LivroRepository repository;
+}
